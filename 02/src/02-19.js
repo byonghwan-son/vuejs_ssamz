f2e43021b033202f353cbf79be58239fd637c7eb
byonghwan-son <sonstein@daum.net>
2023-09-11 23:01:09 +0900

    모듈

---
+++ 02/src/02-19.js
@@ -0,0 +1,11 @@
+/*
+ 모듈 = js 코드를 포함하고 있는 파일
+ import, export 사용
+ 모듈 내부에서 선언된 모든 변수, 함수(함수도 object), 객체, 클래스를 export
+ export 된 모듈은 import 구문으로 참조하여 사용
+ npm run build 후에 사용 가능함
+
+ 모듈을 작성할 때 항상 이용하는 객체, 메서드 등이 있다면
+ export default 구문을 사용하면 편리함.
+ */
+
---
+++ 02/src/02-20-main.js
@@ -0,0 +1,5 @@
+import { add } from './modules/02-19-module'
+import getBase from './modules/02-19-module'
+
+console.log(add(4))
+console.log(getBase())
