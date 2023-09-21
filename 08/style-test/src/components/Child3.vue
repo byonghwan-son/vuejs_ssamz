ce805cf2004e228b0ce9b902fc255b942aa5e54f
byonghwan-son <sonstein@daum.net>
2023-09-21 23:05:49 +0900

    CSS모듈

--- 08/style-test/src/components/Child3.vue
+++ 08/style-test/src/components/Child3.vue
@@ -1,15 +1,18 @@
 <script>
 export default {
-  name: "Child3"
+  name: "Child3",
+  created() {
+    console.log(this.$style)
+  }
 }
 </script>
 
 <template>
-  <div class="child">
+  <div :class="$style.child">
     <h2>Child3</h2>
   </div>
 </template>
 
-<style scoped>
+<style module>
 .child { background-color: orange; border: solid 1px black; margin: 1.5em; padding: 1.0em; }
 </style>
