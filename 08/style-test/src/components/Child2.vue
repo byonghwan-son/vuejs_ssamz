85829de5a1ccb0a7b0d4cb1b416717355c02f44a
byonghwan-son <sonstein@daum.net>
2023-09-21 22:28:59 +0900

    범위 CSS (Scoped CSS)

---
+++ 08/style-test/src/components/Child2.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "Child2"
+}
+</script>
+
+<template>
+  <div class="child">
+    <h2>Child2</h2>
+  </div>
+</template>
+
+<style scoped>
+.child { background-color: blue; border: solid 1px black; margin: 1.5em; padding: 1.0em; }
+</style>
---
+++ 08/style-test/src/components/Child3.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "Child3"
+}
+</script>
+
+<template>
+  <div class="child">
+    <h2>Child3</h2>
+  </div>
+</template>
+
+<style scoped>
+.child { background-color: orange; border: solid 1px black; margin: 1.5em; padding: 1.0em; }
+</style>
