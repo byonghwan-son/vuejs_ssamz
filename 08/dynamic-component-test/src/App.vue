fdb0ae2eba0aa3a6e0a4f40c8051e4316206e2ea
byonghwan-son <sonstein@daum.net>
2023-09-23 14:43:06 +0900

    동적 컴포넌트(dynamic component)

---
+++ 08/dynamic-component-test/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="header">
+    <h1 class="headerText"></h1>
+    <nav>
+      <ul class="nav nav-tabs nav-fill">
+        <li v-for="tab in tabs" :key="tab.id" class="nav-item">
+          <a style="cursor: pointer;" class="nav-link"
+            :class="{ active: tab.id === currentTab }"
+             @click="changeTab(tab.id)"
+          >{{ tab.label }}</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+  <div class="container">
+    <keep-alive include="CoralSeaTab">
+      <component :is="currentTab"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import CoralSeaTab from "@/components/CoralSeaTab.vue";
+import LeyteGulfTab from "@/components/LeyteGulfTab.vue";
+import MidWayTab from "@/components/MidWayTab.vue";
+
+export default {
+  name: "App",
+  components: { CoralSeaTab, LeyteGulfTab, MidWayTab },
+  data() {
+    return {
+      currentTab: 'CoralSeaTab',
+      tabs: [
+        { id:'CoralSeaTab', label: '산호해 해전' },
+        { id:'MidWayTab', label: '미드웨이 해전' },
+        { id:'LeyteGulfTab', label: '레이테만 해전' },
+      ]
+    }
+  },
+  methods: {
+    changeTab(tab) {
+      this.currentTab = tab;
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .header {
+    padding: 20px 0 0 20px;
+  }
+  .headerText {
+    padding: 0px 20px 40px 20px;
+  }
+  .tab {
+    padding: 30px;
+  }
+</style>
---
+++ 08/dynamic-component-test/src/components/CoralSeaTab.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="tab">
+    <h1>산호해 해전에 대해</h1>
+    <div>{{ (new Date().toTimeString()) }}</div>
+    <p>정적 컨텐츠 - keep-alive 를 사용함.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CoralSeaTab"
+}
+</script>
