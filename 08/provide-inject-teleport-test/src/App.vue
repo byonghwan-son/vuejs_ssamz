745eeb1bd2b073d943d7a7b7b52c4f856a5f224d
byonghwan-son <sonstein@daum.net>
2023-09-23 21:18:03 +0900

    텔레포트

--- 08/provide-inject-teleport-test/src/App.vue
+++ 08/provide-inject-teleport-test/src/App.vue
@@ -2,16 +2,22 @@
   <div>
     <h2>최신 인기곡</h2>
     <SongList :songs="songs"/>
+    <br><br>
+    <button @click="changeModal">Teleport를 이용한 Modal 기능</button>
+    <teleport to="#modal">
+      <Modal v-if="isModal" />
+    </teleport>
   </div>
 </template>
 
 <script>
 import SongList from "@/components/SongList.vue";
 import {computed} from "vue";
+import Modal from "@/components/Modal.vue";
 
 export default {
   name: "App",
-  components: {SongList},
+  components: {Modal, SongList},
   data() {
     return {
       songs: [
@@ -19,7 +25,14 @@ export default {
         {id: 2, title: "Dynamite", done: true},
         {id: 3, title: "Lovesick Girls", done: false},
         {id: 4, title: "마리아(Maria)", done: false},
-      ]
+      ],
+      isModal: false,
+    }
+  },
+  methods: {
+    changeModal() {
+      this.isModal = true
+      setTimeout(() => { this.isModal = false }, 2000)
     }
   },
   provide() {
