7c422d144611f4a70260b8f03e058e2f3ce3b0d6
byonghwan-son <sonstein@daum.net>
2023-10-03 00:12:00 +0900

    axios를 이용한 HTTP 통신

---
+++ 11/axios-test-app/src/App4.vue
@@ -0,0 +1,11 @@
+<script setup>
+import axios from "axios";
+
+const requestAPI = async() => {
+  const url = "/api/todolist/gdhong"
+  const response = await axios.get(url)
+  console.log("# 응답객체 : ", response)
+}
+
+requestAPI()
+</script>
---
+++ 11/axios-test-app/src/App6.vue
@@ -0,0 +1,16 @@
+<script setup>
+import axios from "axios";
+
+const requestAPI = async () => {
+  const url = "/api/todolist_long/gdhong"
+  try {
+    const response = await axios.get(url, { timeout: 900 })
+    console.log("# 응답객체 : ", response)
+  } catch(e) {
+    console.log("## 다음 오류가 발생했습니다.")
+    if (e instanceof Error) console.log(e.message)
+    else console.log(e)
+  }
+}
+requestAPI()
+</script>
