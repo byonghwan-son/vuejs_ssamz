6876390ab2d9eee6f9b8d6072b5503e76fe0da86
byonghwan-son <sonstein@daum.net>
2023-10-03 21:26:37 +0900

    3단계 : 비동기 호출로 인한 지연시간 화면(스피너 UI) 변경

--- 12/todolist-app-router/src/App.vue
+++ 12/todolist-app-router/src/App.vue
@@ -2,12 +2,14 @@
   <div class="container">
     <Header/>
     <router-view/>
+    <Loading v-if="states.isLoading"/>
   </div>
 </template>
 
 <script setup>
-import Header from "@/components/Header.vue"
 import {reactive, computed, provide} from "vue"
+import Header from "@/components/Header.vue"
+import Loading from "@/components/Loading.vue";
 import axios from "axios"
 
 const owner = "byonghwan"
@@ -21,9 +23,10 @@ const BASEURI = "/api/todolist_long"
 //     {id: 4, todo: '야구경기 관람', desc: '설명4', done: false},
 //   ]
 // })
-const states = reactive({todoList: []})
+const states = reactive({todoList: [], isLoading: false})
 // TodoList 목록을 조회합니다.
 const fetchTodoList = async () => {
+  states.isLoading = true
   try {
     const response = await axios.get(BASEURI + `/${owner}`)
     if (response.status === 200) {
@@ -34,10 +37,12 @@ const fetchTodoList = async () => {
   } catch (e) {
     alert('에러발생 :' + e)
   }
+  states.isLoading = false
 }
 
 // 새로운 아이템 추가
 const addTodo = async ({todo, desc}, successCallback) => {
+  states.isLoading = true
   try {
     const payload = {todo, desc}
     const response = await axios.post(BASEURI + `/${owner}`, payload)
@@ -50,51 +55,57 @@ const addTodo = async ({todo, desc}, successCallback) => {
   } catch (e) {
     alert('에러발생 : ' + e)
   }
+  states.isLoading = false
 }
 
 const updateTodo = async ({id, todo, desc, done}, successCallback) => {
+  states.isLoading = true
   try {
-    const payload = { todo, desc, done }
+    const payload = {todo, desc, done}
     const response = await axios.put(BASEURI + `/${owner}/${id}`, payload)
-    if(response.data.status === "success") {
+    if (response.data.status === "success") {
       let index = states.todoList.findIndex((todo) => todo.id === id)
       states.todoList[index] = {id, todo, desc, done}
       successCallback()
     } else {
       alert('Todo 변경 실패 : ' + response.data.message)
     }
-  } catch(e) {
+  } catch (e) {
     alert('에러 발생 :' + e)
   }
+  states.isLoading = false
 }
 
 const deleteTodo = async (id) => {
+  states.isLoading = true
   try {
     const response = await axios.delete(BASEURI + `/${owner}/${id}`)
-    if(response.data.status === "success") {
+    if (response.data.status === "success") {
       let index = states.todoList.findIndex((todo) => todo.id === id)
       states.todoList.splice(index, 1)
-    }
-    else {
+    } else {
       alert('Todo 삭제 실패 : ' + response.data.message)
     }
-  } catch(e) {
+  } catch (e) {
     alert('에러발생 :' + e)
   }
+  states.isLoading = false
 }
 
 const toggleDone = async (id) => {
+  states.isLoading = true
   try {
     const response = await axios.put(BASEURI + `/${owner}/${id}/done`)
-    if(response.data.status === "success") {
+    if (response.data.status === "success") {
       let index = states.todoList.findIndex((todo) => todo.id === id)
       states.todoList[index].done = !states.todoList[index].done
     } else {
       alert('Todo 완료 변경 실패: ' + response.data.message)
     }
-  } catch(e) {
+  } catch (e) {
     alert('에러발생 :' + e)
   }
+  states.isLoading = false
 }
 
 // 상태 데이터를 provide 에 주입할 때 computed()를 이용해 계산된 속성으로 처리하여
@@ -102,4 +113,6 @@ const toggleDone = async (id) => {
 // 반응성(reactivity)을 읽어버림.
 provide('todoList', computed(() => states.todoList))
 provide('actions', {addTodo, deleteTodo, toggleDone, updateTodo, fetchTodoList})
+
+fetchTodoList()
 </script>
